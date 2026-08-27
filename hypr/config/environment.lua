8537d9ffa4eaf0eb96ee69468de2ce4aafcf5c82
NorielSylvire <fl4v1u5@yandex.com>
2026-08-27 15:50:54 +0200

    New commit

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -11,3 +11,8 @@ hl.env("GRIMBLAST_EDITOR", "satty --filename")
 
 -- Qt theming
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+
+-- Paths
+local home = os.getenv("HOME")
+
+hl.env("PATH", home .. "/.config/hypr/scripts:" .. os.getenv("PATH"))
